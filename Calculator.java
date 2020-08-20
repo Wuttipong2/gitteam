7b7fab4b6cdcc1487c007ff0315e1a8761abebe8
Wuttipong Phongam <614259046@webmail.npru.ac.th>
2020-08-20 17:23:10 +0700

    Implement Add

--- Calculator.java
+++ Calculator.java
@@ -1,14 +1,14 @@
 public class  Calculator{
            public int add(int num1,int num2){
-           return 0;  
-           }   
+           return num1+num2;
+           }
            public int multiply(int num1, int num2){
            return 0;
            }
-           public int subtract(int num1, int num2){ 
+           public int subtract(int num1, int num2){
            return 0;
            }
-           public double divide(int num1, int num2){ 
+           public double divide(int num1, int num2){
            return 0;
            }
 }
